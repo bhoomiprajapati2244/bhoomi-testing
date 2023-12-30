baa22e2d418c06985c2f0f2ce5a922693b79cb54
bhoomiprajapati2244 <139205701+bhoomiprajapati2244@users.noreply.github.com>
2023-12-30 14:13:35 +0530

    at

---
+++ automation testing/Assignment/Demo18.java
@@ -0,0 +1,12 @@
+package Assignment;
+
+public class Demo18 
+{
+	public static void main(String[] args) 
+	{
+		String str1="Bhoomi";
+		String str2=" Prajapati";
+		String result=str1+str2;
+		System.out.println("concatenated string are:"+result);
+	}
+}
